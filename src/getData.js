01ba8ca30af29cbc83a688390b99d1ef8e737753
Joako27 <55651737+Joako27@users.noreply.github.com>
2024-02-26 19:54:12 +0100

    Add files via upload

--- src/getData.js
+++ src/getData.js
@@ -54,20 +54,170 @@ function mostraStats(item, opc) {
         var languages = arr[3];
         var autor_name = arr[0];
         var subjects = arr[11];
-        miDiv.innerHTML = '<h1>Resultados:</h1>';
-        miDiv2.innerHTML += '<div><h2>Título: ' + title + '</h2></div>' +
-        '<div>ISBN:' + id + '</div>' + 
-        '<div>Fecha de Lanzamiento:' + publish_date + '</div>' +
-        '<div>Páginas:' + pages + '</div>' +
-        '<div>Número de Ediciones:' + num_editions + '</div>' +
-        '<div>Contribuidores:' + contributors + '</div>' +
-        '<div>Lugares en los que se publicó:' + placepublish + '</div>' +
-        '<div>Puntuación:' + rating + '</div>' +
-        '<div>Usuarios que quieren leer este libro:' + quieren_leer + '</div>' +
-        '<div>Usuarios que ya lo estan leyendo:' + ya_leyendo + '</div>' +
-        '<div>Publicadores:' + publishers + '</div>' +
-        '<div>Idiomas:' + languages + '</div>'  +
-        '<div>Nombre del Autor:' + autor_name + '</div>' +
-        '<div>Categorías:' + subjects + '</div>';
+        miDiv.innerHTML = '<h1 style="text-align: center;">Resultados:</h1>';
+        miDiv2.innerHTML += '<div style="background-color: rgba(54, 162, 235, 0.5); color: black; border-radius: 5px; padding: 3%; margin-bottom: 3%;">' + 
+        '<div style="background-color: rgba(255, 99, 132, 1); color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><h2>Título: ' + title + '</h2></div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>ID:</b>' + id + '</div>' + 
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Fecha de Lanzamiento:</b>' + publish_date + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Páginas:</b>' + pages + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Número de Ediciones:</b>' + num_editions + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Contribuidores:</b>' + contributors + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Lugares en los que se publicó:</b>' + placepublish + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Puntuación:</b>' + rating + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Usuarios que quieren leer este libro:</b>' + quieren_leer + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Usuarios que ya lo estan leyendo:</b>' + ya_leyendo + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Publicadores:</b>' + publishers + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Idiomas:</b>' + languages + '</div>'  +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Nombre del Autor:</b>' + autor_name + '</div>' +
+        '<div style="background-color: white; color: black; border-radius: 5px; padding: 1%; margin-bottom: 3%;"><b>Categorías:</b>' + subjects + '</div></div>';
     }
 }
+function miFuncion() {
+    var labelStat = []
+    var data1 = []
+    var data2 = []
+    fetch('http://127.0.0.1:5000/stats1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+            for (const item of data) {
+                labelStat.push(item["Title"])
+                data1.push(item["QuierenLeer"])
+                data2.push(item["YaLeyendo"])
+            }
+            miDiv2 = document.getElementById("stats1");
+            miDiv2.innerHTML = '<canvas id="grafico" width="50%" height="50%" style="margin-left: 20%; margin-right: 20%;"></canvas>';
+            let canvas = document.getElementById("grafico");
+            var data = {
+                labels: labelStat,
+                datasets: [{
+                    label: 'Personas que quieren leer el libro',
+                    data: data1,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)', 
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }, {
+                    label: 'Personas que ya estan leyendo el libro',
+                    data: data2,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)', 
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            };
+            graph = new Chart(canvas, {
+                type: "bar",
+                data: data
+            });   
+        });
+        doGraph();
+        doGraph2();
+}
+
+function doGraph(){
+    var ratg = []
+    var counter = 0
+    var dataToSend = {
+        val: "",
+    };
+    fetch('http://127.0.0.1:5000/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+        })
+        .then(response => response.json())
+        .then(data => {
+            for (const item of data) {
+                ratg.push([item["Title"], item["Rating"]])
+            }
+            maxRating = []
+            nameTitle = []
+            ratg.sort(function(a, b) {
+                return b[1] - a[1];
+            });
+            for (var i in ratg){
+                maxRating.push(ratg[i][1])
+                nameTitle.push(ratg[i][0])
+                if(counter == 4){
+                    break;
+                }
+                counter++;
+            }
+            miDiv2 = document.getElementById("stats2");
+            miDiv2.innerHTML = '<canvas id="grafico2" width="50%" height="50%" style="margin-left: 30%; margin-right: 30%;"></canvas>';
+            let canvas = document.getElementById("grafico2");
+            var data = {
+                labels: nameTitle,
+                datasets: [{
+                    label: 'Ratings',
+                    data: maxRating,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)', 
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            };
+            graph = new Chart(canvas, {
+                type: "bar",
+                data: data
+            });   
+
+    });
+}
+
+function doGraph2(){
+    var pag = []
+    var counter = 0
+    var dataToSend = {
+        val: "",
+    };
+    fetch('http://127.0.0.1:5000/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+        })
+        .then(response => response.json())
+        .then(data => {
+            for (const item of data) {
+                pag.push([item["Title"], item["Pages"]])
+            }
+            maxPages = []
+            nameTitle = []
+            pag.sort(function(a, b) {
+                return b[1] - a[1];
+            });
+            for (var i in pag){
+                maxPages.push(pag[i][1])
+                nameTitle.push(pag[i][0])
+                if(counter == 4){
+                    break;
+                }
+                counter++;
+            }
+            miDiv2 = document.getElementById("stats3");
+            miDiv2.innerHTML = '<canvas id="grafico3" width="50%" height="50%" style="margin-left: 30%; margin-right: 30%;"></canvas>';
+            let canvas = document.getElementById("grafico3");
+            var data = {
+                labels: nameTitle,
+                datasets: [{
+                    label: 'Listado de los 5 libros con mayor número de páginas',
+                    data: maxPages,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)', 
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            };
+            graph = new Chart(canvas, {
+                type: "bar",
+                data: data
+            });   
+
+    });
+}
+
